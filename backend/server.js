56b327cba2eec33af1f278520fa38166bea344c4
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-04 18:01:34 -0500

    created server and added error middlewares

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./db');
+const middlewares = require('./middlewares');
 
 console.log('__dirname', __dirname);
 dotenv.config();
@@ -15,6 +16,14 @@ app.use(express.json());
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' })
+  // res.send('API is running')
+})
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
